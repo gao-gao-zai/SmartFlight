078e34790c4fcbca1b24800ca58b8c1c035e79b1
gao-gao-zai <gao_gao_zai@outlook.com>
2026-05-13 21:43:27 +0800

    Add ABI split release outputs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,6 +41,15 @@ android {
         }
     }
 
+    splits {
+        abi {
+            isEnable = true
+            reset()
+            include("armeabi-v7a", "arm64-v8a")
+            isUniversalApk = false
+        }
+    }
+
     buildTypes {
         release {
             if (keystorePropertiesFile.exists()) {
